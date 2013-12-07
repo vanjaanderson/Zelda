ba50c7eb24a6e9bf6dfed808579299c8e3e737f8
Vanja Anderson <vanja@websnabben.se>
2013-12-07 19:22:45 +0100

    En modulhanterare till Zelda

---
+++ src/CCModules/index.tpl.php
@@ -0,0 +1,29 @@
+<h1>Modulhanterare</h1>
+
+
+<h2>Om modulhanteraren</h2>
+<p>
+	<strong>Modulhanteraren</strong> visar information om vilka moduler som finns i Zelda, och hanterar dessa. Zelda är uppbyggt av moduler och varje modul har sin 
+	egen mapp i <code>src</code>-mappen.
+</p>
+
+<h2>Aktiverade controllers</h2>
+<p>
+	Controllerna skapar det lokala API:t för denna webbplats. Här är en lista över alla aktiva controllers och deras metoder. Du aktiverar och/eller inaktiverar controllers i  
+	<code>site/config.php</code>.
+</p>
+
+<ul>
+	<?php foreach($controllers as $key => $val): ?>
+	  <li><a href='<?=create_url($key)?>'><?=$key?></a></li>
+
+	  <?php if(!empty($val)): ?>
+	  <ul>
+		  <?php foreach($val as $method): ?>
+		    <li><a href='<?=create_url($key, $method)?>'><?=$method?></a></li> 
+		  <?php endforeach; ?>		
+	  </ul>
+	  <?php endif; ?>
+	  
+	<?php endforeach; ?>		
+</ul>
---
+++ src/CCModules/sidebar.tpl.php
@@ -0,0 +1,81 @@
+<div class='box'>
+<h4>Alla moduler</h4>
+<p><em>Alla Zeldas moduler:</em></p>
+  <ul>
+    <?php foreach($modules as $module): ?>
+      <li><?=$module['name']?></li>
+    <?php endforeach; ?>
+  </ul>
+</div>
+
+<div class='box'>
+<h4>Zelda Core</h4>
+<p><em>Moduler i Zelda Core:</em></p>
+  <ul>
+    <?php foreach($modules as $module): ?>
+      <?php if($module['isZeldaCore']): ?>
+      <li><?=$module['name']?></li>
+      <?php endif; ?>
+    <?php endforeach; ?>
+  </ul>
+</div>
+
+<div class='box'>
+<h4>Zelda CMF</h4>
+<p><em>Lydia Content Management Framework (CMF) moduler:</em></p>
+  <ul>
+    <?php foreach($modules as $module): ?>
+      <?php if($module['isZeldaCMF']): ?>
+      <li><?=$module['name']?></li>
+      <?php endif; ?>
+    <?php endforeach; ?>
+  </ul>
+</div>
+
+<div class='box'>
+<h4>Modeller</h4>
+<p><em>En klass som innehåller en modell börjar på CM:</em></p>
+  <ul>
+    <?php foreach($modules as $module): ?>
+      <?php if($module['isModel']): ?>
+      <li><?=$module['name']?></li>
+      <?php endif; ?>
+    <?php endforeach; ?>
+  </ul>
+</div>
+
+<div class='box'>
+<h4>Controllers</h4>
+<p><em>En controller implementerar interfacet <code>IController</code>:</em></p>
+  <ul>
+    <?php foreach($modules as $module): ?>
+      <?php if($module['isController']): ?>
+      <li><?=$module['name']?></li>
+      <?php endif; ?>
+    <?php endforeach; ?>
+  </ul>
+</div>
+
+<div class='box'>
+<h4>Innehåller SQL</h4>
+<p><em>Implementerar interfacet <code>IHasSQL</code>:</em></p>
+  <ul>
+    <?php foreach($modules as $module): ?>
+      <?php if($module['hasSQL']): ?>
+      <li><?=$module['name']?></li>
+      <?php endif; ?>
+    <?php endforeach; ?>
+  </ul>
+</div>
+
+<div class='box'>
+<h4>Fler moduler</h4>
+<p><em>Moduler som inte implementerar något interface i Zelda:</em></p>
+  <ul>
+    <?php foreach($modules as $module): ?>
+      <?php if(!($module['isController'] || $module['isZeldaCore'] || $module['isZeldaCMF'])): ?>
+      <li><?=$module['name']?></li>
+      <?php endif; ?>
+    <?php endforeach; ?>
+  </ul>
+</div>
