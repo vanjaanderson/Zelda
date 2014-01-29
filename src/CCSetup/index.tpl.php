daae8074673a71d104bb5523089cfbe0a0f57cc0
Vanja Anderson <vanja@websnabben.se>
2014-01-29 21:38:57 +0100

    For examination

--- src/CCSetup/index.tpl.php
+++ src/CCSetup/index.tpl.php
@@ -17,21 +17,30 @@ else {
 	$check1 = '<p class="success">Version of PHP is ('.phpversion().'). <span style="float:right;font-style:italic;font-weight:bold;color:green;">OK</span></p>';
 }
 
-// Check 3: Check if data directory is writable.
+// Check 2: Check if data directory is writable.
 if (!is_writable('site/data')) {
-	$check3 = '<p class="error">Directory site/data must be writable. <span style="float:right;font-style:italic;font-weight:bold;color:red;">&mdash;</span><br />
+	$check2 = '<p class="error">Directory site/data must be writable. <span style="float:right;font-style:italic;font-weight:bold;color:red;">&mdash;</span><br />
 	<a href=' . $_SERVER['PHP_SELF'] . '>Load page to check again</a></p>';
 }
 else {
-	$check3 = '<p class="success">Directory site/data is writable. <span style="float:right;font-style:italic;font-weight:bold;color:green;">OK</span></p>';
+	$check2 = '<p class="success">Directory site/data is writable. <span style="float:right;font-style:italic;font-weight:bold;color:green;">OK</span></p>';
 }
 
-// Check 2: Check if you can use database att all.
-if (!defined('PDO::ATTR_DRIVER_NAME')) {
-	$check2 = '<p class="error">Sorry, you can not run sqlite on this environment! <span style="float:right;font-style:italic;font-weight:bold;color:red;">&mdash;</span>';
+// Check 3: Check if database is writable.
+if (!is_writable('site/data/.ht.sqlite')) {
+	$check3 = '<p class="error">Database site/data/.ht.sqlite must be writable. <span style="float:right;font-style:italic;font-weight:bold;color:red;">&mdash;</span><br />
+	<a href=' . $_SERVER['PHP_SELF'] . '>Load page to check again</a></p>';
 }
 else {
-	$check2 = '<p class="success">You may run sqlite on this web server. <span style="float:right;font-style:italic;font-weight:bold;color:green;">OK</span></p>';
+	$check3 = '<p class="success">Database (.ht.sqlite) is writable. <span style="float:right;font-style:italic;font-weight:bold;color:green;">OK</span></p>';
+}
+
+// Check 4: Check if you can use database att all.
+if (!defined('PDO::ATTR_DRIVER_NAME')) {
+	$check4 = '<p class="error">Sorry, you can not run sqlite on this environment! <span style="float:right;font-style:italic;font-weight:bold;color:red;">&mdash;</span>';
+}
+else {
+	$check4 = '<p class="success">You may run sqlite on this web server. <span style="float:right;font-style:italic;font-weight:bold;color:green;">OK</span></p>';
 }
 
 ?>
@@ -43,7 +52,7 @@ else {
 <h3>Requirements</h3>
 <p>To run this framework, you need a web server (Apache) with PHP version of 5.3 or higher. Database used, is sqlite, and is automatically installed in the framework.</p>
 <?=$check1?>
-<?=$check2?>
+<?=$check4?>
 
 <h3>Instructions for installation</h3>
 <ol>
@@ -53,12 +62,13 @@ else {
 	<code>git clone git://github.com/vanjaanderson/Zelda.git</code>
 </blockquote>
 
-<li>Put files in desired directory on your web server, and make sure the <code>site/data</code> directory is writable. In your terminal, write command:</li>
+<li>Put files in desired directory on your web server, and make sure the <code>site/data</code> directory, and database in it, are writable. In your terminal, write command:</li>
 
 <blockquote>
-	<code>cd Zelda; chmod 777 site/data</code>
+	<code>cd Zelda; chmod -R 777 site/data</code>
 </blockquote>
 
+<?=$check2?>
 <?=$check3?>
 
 <li>Uncomment row <code style="color:gray">#RewriteBase /Zelda/</code> to <code style="color:gray">RewriteBase /Zelda/</code> in file .htaccess, if needed.</li>
